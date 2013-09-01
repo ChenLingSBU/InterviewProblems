0df075a008c47411c23d0ce6ad2d7da217be3112
Chen Ling <chen.ling.1@stonybrook.edu>
2013-09-01 11:18:48 -0400

    add problem of replace %20 with space in a String.

---
+++ InterveiwProblems/CodingInterviewProblem/src/other/Replace20WithSpace.java
@@ -0,0 +1,53 @@
+package other;
+
+public class Replace20WithSpace {
+	
+	public static String replace(String strIn){
+		char[] charArray = strIn.toCharArray();
+		int counter = counterChangePostions(charArray, 0);
+		int originalLength = charArray.length;
+		int newLength = originalLength - 2*counter;
+		int changePos;
+		char[] outChar = new char[newLength];
+		for(int i = 0; i< counter; i++){
+			changePos = findChangePosition(charArray);
+			moveForward(charArray, changePos);
+		}
+		
+		for(int i = 0; i< newLength; i++){
+			outChar[i] = charArray[i];
+		}
+		return new String(outChar);	
+	}
+	
+	public static void moveForward(char[] charArray, int changePos){
+		for(int pos = changePos +1 ; pos + 2 <charArray.length; pos++){
+			charArray[pos] = charArray[pos + 2];
+		}
+	}
+	
+	public static int counterChangePostions(char[] charArray, int counter){
+		for(int i = 0; i< charArray.length; i++){
+			if(charArray[i] == '%'){
+				counter++;
+			}	
+		}
+		return counter;	
+	}
+	
+	public static int findChangePosition(char[] charArray){
+		for(int pos = 0; pos< charArray.length; pos++){
+			if(charArray[pos] == '%'){
+				charArray[pos] = ' ';
+				return pos;
+			}	
+		}
+		return charArray.length - 1; //not find change position
+	}
+
+	public static void main(String[] args) {
+		String str = "Mr%20John%20smith.";
+		System.out.println(Replace20WithSpace.replace(str));
+	}
+
+}
