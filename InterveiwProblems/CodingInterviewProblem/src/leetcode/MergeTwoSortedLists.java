c774140126fb13f20f55c8ec92cb71006f69c73b
Chen Ling <chen.ling.1@stonybrook.edu>
2013-09-05 12:02:37 -0400

    add heap algorithm for solution

--- InterveiwProblems/CodingInterviewProblem/src/leetcode/MergeTwoSortedLists.java
+++ InterveiwProblems/CodingInterviewProblem/src/leetcode/MergeTwoSortedLists.java
@@ -1,13 +1,19 @@
 package leetcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /*
- * note, make use of "sorted"
- * we can create a new list with head node, and add proper node after head one by one. 
+ * note: here lists two algorithms for solution
+ * 1.  make use of "sorted"
+ * 		we can create a new list with head node, and add proper node after head one by one. 
+ * 
+ * 2. use Heap. 
  */
 import leetcodeLib.ListNode;
 
 public class MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoListsTwoPointers(ListNode l1, ListNode l2) {
         if(l1==null && l2==null)
             return null;
         
@@ -34,6 +40,46 @@ public class MergeTwoSortedLists {
             
         return head.next;
     }
+    
+    
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+        	return null;
+
+Comparator<ListNode> comparator = new Comparator<ListNode>() {
+	public int compare(ListNode n1, ListNode n2) {
+		if (n1.val < n2.val)
+			return -1;
+		else if (n1.val > n2.val)
+			return 1;
+		else
+			return 0;
+		}
+	};
+
+	PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(2, comparator);
+	ListNode head = null;
+	ListNode cur = null;
+	if(l1!=null)
+		heap.add(l1);
+	if(l2!=null)
+		heap.add(l2);
+	while(!heap.isEmpty()) {
+		if (head == null) {
+			head = heap.poll();
+			cur = head;
+			if (cur.next != null)
+				heap.add(cur.next);
+			} else {
+				ListNode newNode = heap.poll();
+				cur.next = newNode;
+				cur = newNode;
+				if (cur.next != null)
+					heap.add(cur.next);
+				}
+		}
+	return head;
+	}
 	
 	public static void main(String[] args) {
 
