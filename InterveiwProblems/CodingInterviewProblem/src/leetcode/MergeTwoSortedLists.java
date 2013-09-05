1ff9441eaba159790b727950080d015ca5881e5c
Chen Ling <chen.ling.1@stonybrook.edu>
2013-09-05 12:04:18 -0400

    update heap

--- InterveiwProblems/CodingInterviewProblem/src/leetcode/MergeTwoSortedLists.java
+++ InterveiwProblems/CodingInterviewProblem/src/leetcode/MergeTwoSortedLists.java
@@ -42,7 +42,7 @@ public class MergeTwoSortedLists {
     }
     
     
-public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+public ListNode mergeTwoListsHeap(ListNode l1, ListNode l2) {
         if (l1 == null && l2 == null)
         	return null;
 
