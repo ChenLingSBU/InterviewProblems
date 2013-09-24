6efa9908f2dba72ba678e3c1b5a0e28d3c897f18
Chen Ling <chen.ling.1@stonybrook.edu>
2013-09-24 01:46:36 -0400

    Edit Distance

--- InterveiwProblems/CodingInterviewProblem/src/leetcode/EditDistance.java
+++ InterveiwProblems/CodingInterviewProblem/src/leetcode/EditDistance.java
@@ -25,14 +25,13 @@ public class EditDistance {
             dp[0][i] = i;
         }
         
-        
         for(int i = 1; i <= word1.length(); i++){
             for(int j = 1; j <= word2.length(); j++){
                 dp[i][j] = Integer.MAX_VALUE;
                 if(word1.charAt(i - 1) == word2.charAt(j - 1))
-                   dp[i][j] = dp[i - 1][j - 1];
-                dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1] + 1);
-                dp[i][j] = Math.min(dp[i][j], Math.min(dp[i - 1][j], dp[i][j - 1]) + 1);
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]) + 1);
+                else
+                   dp[i][j] = Math.min(dp[i-1][j-1] + 1, Math.min(dp[i - 1][j], dp[i][j - 1]) + 1);
             }
         }
         
