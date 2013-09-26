81818c86c1297e1154a941a3ff09a368b6b1e44f
Chen Ling <chen.ling.1@stonybrook.edu>
2013-09-25 21:06:25 -0400

    Regular Expression Matching.

---
+++ InterveiwProblems/CodingInterviewProblem/src/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+
+/*
+ * DP version will be updated soon. 
+ */
+
+public class RegularExpressionMatching {
+
+	   public static boolean isMatch(String s, String p) {
+		   
+	        return isMatch(s.toCharArray(), p.toCharArray(), 0, 0);
+	    }
+	    
+	    public static boolean isMatch(char[] s, char[] p, int sIndex, int pIndex){
+	        if(pIndex == p.length) return sIndex == s.length;
+	        
+	        if(pIndex == p.length - 1 || p[pIndex + 1] != '*'){
+	            if(sIndex < s.length && (p[pIndex] == s[sIndex] || p[pIndex] == '.') )
+	                return isMatch(s, p, sIndex + 1, pIndex + 1);
+	            return false;
+	        }
+	        else {
+	            while(sIndex < s.length && (p[pIndex] == s[sIndex] || p[pIndex] == '.' )){
+	                if(isMatch(s, p, sIndex, pIndex + 2))
+	                    return true;
+	                sIndex++;
+	            }
+	            
+	            return isMatch(s, p, sIndex, pIndex + 2);
+	        }
+	        
+	    }
+	    
+	    
+	public static void main(String[] args) {
+			String s = "aa";
+			String p = "a";
+			System.out.println(RegularExpressionMatching.isMatch(s, p));
+	}
+}
