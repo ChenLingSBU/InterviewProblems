c406e0ac19aebd5152ddba7a05334b56f7c3e9c4
Chen Ling <chen.ling.1@stonybrook.edu>
2013-10-14 17:25:27 -0400

    Data Structure of Interval

---
+++ InterveiwProblems/CodingInterviewProblem/src/leetcodeLib/Interval.java
@@ -0,0 +1,8 @@
+package leetcodeLib;
+
+public class Interval {
+	    public int start;
+	    public  int end;
+	      Interval() { start = 0; end = 0; }
+	      Interval(int s, int e) { start = s; end = e; }
+	  }
