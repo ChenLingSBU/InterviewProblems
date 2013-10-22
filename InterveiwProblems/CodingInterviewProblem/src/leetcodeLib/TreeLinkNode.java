d5fe2b1d9c0e698e88a4198e19a44f2e111f94a7
Chen Ling <chen.ling.1@stonybrook.edu>
2013-10-22 01:48:09 -0400

    TreeLinkNode data structure

---
+++ InterveiwProblems/CodingInterviewProblem/src/leetcodeLib/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package leetcodeLib;
+
+public class TreeLinkNode {
+
+	     public int val;
+	    public TreeLinkNode left, right, next;
+	     public TreeLinkNode(int x) { val = x; }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
