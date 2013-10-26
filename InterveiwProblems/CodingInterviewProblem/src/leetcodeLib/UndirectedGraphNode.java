fab9b6dce306f5900b1f53f36f3b283cbd3f7905
Chen Ling <chen.ling.1@stonybrook.edu>
2013-10-25 22:59:11 -0400

    UndirectedGraphNode

---
+++ InterveiwProblems/CodingInterviewProblem/src/leetcodeLib/UndirectedGraphNode.java
@@ -0,0 +1,15 @@
+package leetcodeLib;
+
+import java.util.ArrayList;
+public class UndirectedGraphNode {
+
+	   public int label;
+	   public ArrayList<UndirectedGraphNode> neighbors;
+	   public UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
