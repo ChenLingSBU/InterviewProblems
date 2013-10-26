3107b461656ec9c51acc4ce9ade90ca997c258df
Chen Ling <chen.ling.1@stonybrook.edu>
2013-10-25 23:55:53 -0400

    RandomListNode

---
+++ InterveiwProblems/CodingInterviewProblem/src/leetcodeLib/RandomListNode.java
@@ -0,0 +1,13 @@
+package leetcodeLib;
+
+
+
+public class RandomListNode {
+
+	 public int label;
+	 public RandomListNode next, random;
+	 public RandomListNode(int x) { this.label = x; }
+	public static void main(String[] args) {
+	
+	}
+}
