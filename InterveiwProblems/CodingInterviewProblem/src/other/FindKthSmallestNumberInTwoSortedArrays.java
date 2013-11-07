50eb2ddd24e4450ea9ff6c9055ee54f9c2fee34f
ChenLing <gameboylc@gmail.com>
2013-11-07 08:06:26 -0500

    find kth smallest number in two sroted arrays

---
+++ InterveiwProblems/CodingInterviewProblem/src/other/FindKthSmallestNumberInTwoSortedArrays.java
@@ -0,0 +1,13 @@
+package other;
+
+
+/*
+ * the same as find median of two sorted Arrays. 
+ */
+public class FindKthSmallestNumberInTwoSortedArrays {
+
+	
+	public static void main(String[] args) {
+
+	}
+}
