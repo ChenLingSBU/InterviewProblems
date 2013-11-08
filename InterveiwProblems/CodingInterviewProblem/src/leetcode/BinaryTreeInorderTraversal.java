e24223fb97fb5283c3d2eba1ef9e9ae292d8482f
Chen Ling <chen.ling.1@stonybrook.edu>
2013-11-08 13:10:03 -0500

    Binary Tree Inorder Traversal

--- InterveiwProblems/CodingInterviewProblem/src/leetcode/BinaryTreeInorderTraversal.java
+++ InterveiwProblems/CodingInterviewProblem/src/leetcode/BinaryTreeInorderTraversal.java
@@ -23,28 +23,27 @@ import java.util.Stack;
 public class BinaryTreeInorderTraversal {
 
 	public static ArrayList<Integer> inorderTraversal(TreeNode root){
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        if(root == null) return result;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-        	if( root != null&&root.left !=null){
-        		root = root.left;
-        		stack.push(root);
-        	}
-        	else{
-        		root = stack.pop();
-        		result.add(root.val);
-        		if(root.right != null){
-        			root = root.right;
-        			stack.push(root);
-        		}else{
-        			root = null; //don't forget this case. 
-        		}
-        	}
-         }
-        
-        return result;
+	    ArrayList<Integer> result = new ArrayList<Integer>();
+	    if(root == null) return result;
+	    Stack<TreeNode> stack = new Stack<TreeNode>();
+	    TreeNode  node = root;
+	    stack.push(node);
+	    while(!stack.isEmpty()){
+	        if(node != null && node.left != null){ //block 1.  here node != null is used to judge the result of node 1. 
+	            node = node.left;
+	            stack.push(node);
+	        }else{
+	            node = stack.pop();
+	            result.add(node.val);
+	            if(node.right != null){
+	                node = node.right;
+	                stack.push(node);
+	            }else
+	                node = null; // note 1: don't forget this, otherwise can jump over block1. 
+	        }
+	    }
+	    
+	    return result;
 	}
 	
 	public static void main(String[] args) {
