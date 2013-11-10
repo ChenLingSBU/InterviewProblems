3ed56eb393614c12cc3cfb8288ff0e8874689ba4
Chen Ling <chen.ling.1@stonybrook.edu>
2013-11-10 10:15:19 -0500

    DoubleListNode structure

---
+++ InterveiwProblems/CodingInterviewProblem/src/leetcodeLib/DoubleListNode.java
@@ -0,0 +1,17 @@
+package leetcodeLib;
+
+public class DoubleListNode {
+	
+	   public  int val;
+	    public  DoubleListNode left;
+	    public DoubleListNode right;
+	    public  DoubleListNode(int x) {
+	          val = x;
+	          left = null;
+	          right = null;
+	      }
+
+	public static void main(String[] args) {
+	
+	}
+}
