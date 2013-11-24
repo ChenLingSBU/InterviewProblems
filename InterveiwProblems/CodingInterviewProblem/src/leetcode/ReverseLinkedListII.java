99fea77e04b618ce7e3be0fc309a0d11e82b7261
Chen Ling <chen.ling.1@stonybrook.edu>
2013-11-24 18:23:25 -0500

    Reverse Linked ListII

--- InterveiwProblems/CodingInterviewProblem/src/leetcode/ReverseLinkedListII.java
+++ InterveiwProblems/CodingInterviewProblem/src/leetcode/ReverseLinkedListII.java
@@ -3,49 +3,47 @@ package leetcode;
 /*
  * note, according to the description of problem, we know 1<=m<=n<=length;
  * 
- * The idea is straight forward, we find the newHead node which at m, and tail node which at n, and reverse between m and n.
- * There's one buggy spot is don't forget to check this case : m = 1. like note1. 
  */
 
 import leetcodeLib.ListNode;
 public class ReverseLinkedListII {
     public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(head == null) return null;
         
-        if(head == null || head.next == null) return head;
         
-        ListNode pre = new ListNode(Integer.MIN_VALUE);
-        pre.next = head;
-        ListNode newHead = pre.next;
-        ListNode tail = pre.next;
-        int step = 0;
-        while(step < n - 1){
-            tail = tail.next;
-            
-            if(step < m - 1){
-                newHead = newHead.next;
-                pre = pre.next;
-            }
-            
-            step++;
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        ListNode pre = dummyHead;
+        ListNode post;
+        
+        int step = n - m;
+        
+        while(m > 0){
+            cur = cur.next;
+            m--;
         }
         
-        ListNode cur =newHead;
-        ListNode post = cur.next;
-        tail = tail.next;
-        while(post != tail){
-            ListNode temp = post.next;
+        post = cur.next;
+        
+        while(pre.next != cur)
+            pre = pre.next;
+        
+        while(step > 0 && post != null){
+            ListNode tmp = post.next;
             post.next = cur;
             cur = post;
-            post = temp;
+            post = tmp;
+            step--;
         }
-       newHead.next = tail;
-       pre.next = cur;
-       
-       if(newHead == head) //note1.
-            return pre.next;
-        else
-            return head;
+        
+        pre.next.next = post;
+        pre.next = cur;
+        
+        return dummyHead.next;
+        
     }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
